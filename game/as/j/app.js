9ea59c3822b79ddef26f272401cda3b858cb3387
Your Name <you@example.com>
2022-08-10 23:00:21 +0300

    All

--- game/as/j/app.js
+++ game/as/j/app.js
@@ -10,5 +10,15 @@ document.querySelector('.play_btn').onclick = function(){
     document.querySelector('main').append(document_center_selector);
     document.querySelector('main').append(document_bottom_selector);
     wipe(document_center_selector);
-    delay_type(document_center_selector, 'Устраивайтесь поудобнее: в уютном кресле под старым торшером начинается книжная терапия. Что прячется за каждым знаком? Боль, страх — или любовь, которая рушит границы реального? Жуткие и добрые, нежные и шокирующие истории оставят след в душе и заставят по-новому взглянуть на привычное.', 0, 40);
+    let t = delay_type(document_center_selector, 'Устраивайтесь поудобнее: в уютном кресле под старым торшером начинается книжная терапия. Что прячется за каждым знаком? Боль, страх — или любовь, которая рушит границы реального? Жуткие и добрые, нежные и шокирующие истории оставят след в душе и заставят по-новому взглянуть на привычное.', 0, 50);
+    setTimeout(() => console.log(document_bottom_selector.innerHTML = '<a class="play_btn_start">Узнать цитату</a>'), 15000);
+    setTimeout(cl1, 15001);
+}
+
+const cl1 = function(){
+    document.querySelector('.play_btn_start').onclick = function(){
+        wipe(document.querySelector('.d_center_selector'));
+        delay_type(document.querySelector('.d_center_selector'), getZitati().replace('\n', '</br>'), 0, 60);
+        wipe(document.querySelector('.d_bottom_selector'));
+    }
 }
